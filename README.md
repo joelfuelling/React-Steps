13d91e2a00b5ce49a3136de530cf48379edb5bcb
Joel <JoelFuelling@gmail.com>
2023-08-25 16:52:11 -0500

    Steps exercise complete. I added the functionality of changing the displayed class on the step divs to go up/down by 1 depending on which number was clicked

--- README.md
+++ README.md
@@ -3,3 +3,46 @@
 - What is state and why do we need it?
 
 - onSubmit, onMouseEnter, onChange: These are the functions that can be used as a prop to have something happend based on an event. Combining this with the state, we can alter the displayed variables and UI.
+
+# State
+
+- Data that a component can hold over time, necessary for information that it needs to remember throughout the lifecycle.
+- "Componnent Memory"
+- "State variable" / "piece of state": A single variable in a component (component state)
+
+- Whenever we update state in a component, it re-renders that component/view.
+
+--- State allows us to update a components view (by re-rendering it)
+--- State allows local variables to PERSIST BETWEEN RENDERS
+
+- useState() is a "hook" (hook: any react funciton starting with 'use'), which can only be called at the top level of the function component, immediately after declaring it - CANNOT BE CALLED CONDITIONALLY.
+- We should ONLY update state with the setState, setData, setDog, etc.,
+
+------DONT SET STATE MANUALLY---- 
+
+- When we try to updte state manually we break React, and create unintended consequences.
+- ONLY EVER USE the useState() setter functions provided by React for this specific purpose.
+
+# State Mechanics
+
+- 1 We can't do direct DOM manipulations -> 2 - How is a component view updated then? -> 3 - In React, a view is updated by re-rendering the component (*Very important*) -> 4 - A component is re-rendered when its state is updated -> 5 - So to update a view, we update state. 
+
+- React preservers the component state throughout renders, unless the component disappers from the UI entirely.
+
+# Adding another piece of State.
+
+- Each component has and manages ITS OWN STATE, no matter how many times we render the same component.
+
+const [step, setStep] = useState((s) => s = 1)
+
+- UI(component 1, component 2,...) = aFuncitonOf(state)
+
+# When to use State!
+
+- 1 - Any data that the component should keep track of ("remember") over time. This is data that will change at some point. In Vanilla JS, that's a 'let' variable, or an [] or {}
+- 2 - When you want something in the component to be dynamic, create a piece of state related to that "thing", and update the state when "thing" should change (aka "be dynamic")   
+    - isOpen = true vs. isOpen = false
+- 3 - If you want to change the way a component looks, or the data it displays, update its state. This usually happens in an event handler function.
+
+*** For data that should not triggger component re-renders, don't use state! Use a regular variables instead. This is a COMMON BEGINNER MISTAKE.
+
--- src/App.js
+++ src/App.js
@@ -9,45 +9,69 @@ const messages = [
   "Invest your new income 🤑",
 ];
 
+export default function App() {
+  return(
+    <div>
+      {/* State is ISOLATED to each component. Test it out below */}
+    <Steps />
+    <Steps />
+    </div>
+  )
+}
 
+function Steps() {
+  // 1 - Add state variable.
+  const [step, setStep] = useState(1)
+  const [isOpen,setIsOpen] = useState(true)
+  // const [test, setTest] = useState({name: 'Joel'})
 
-function App() {
-  let [step, setState] = useState(1)
-
+  // 3 - Update step variable.
   function handlePrevious() {
-    step -= 1
-    setState(step)
+    if(step > 1) setStep((s) => s -1)
   }
 
+  // 3 - Update step variable.
   function handleNext() {
-    step += 1
-    setState(step)
-  }
-  return (
-    <>
-      <div className="steps">
-        <div className = "numbers"> 
-          <div className={`${step >= 1 ? 'active' : ''}`}>1</div>
-          <div className={`${step >= 2 ? 'active' : ''}`}>2</div> 
-          <div className={`${step >= 3 ? 'active' : ''}`}>3</div>
-        </div>
-      <p className="message">Step {step}: {messages[step -1]}</p>
-        <div className ="buttons">
-          <button 
-          style={buttonStyle} 
-          onClick={handlePrevious}>
-            Previous
-          </button>
-          <button 
-          style={buttonStyle} 
-          onClick={handleNext}>
-            Next
-          </button>
-        </div>
+    if(step < 3) {
+      setStep((s) => s + 1)
+      // setStep((s) => s + 1) Here, state will be called twice because we're using a callback to set it!
+    }
 
+    // step = step + 1 //! WRONG! This does not update state because setStep is not being called.
+    // test.name = "Fred" //! This WILL update the state object test {name: "Joel"}, which is bad practice and very dangerous!
+    // setTest({name: "Fred"})  //* Here we go! We're using state correctly to change the name to Fred in the re-render, not the actual object.
+  }
+  return ( 
+    <>
+    <div>
+      <button className="close" onClick={() => setIsOpen((is) => !is) }>&times;</button>
+      {isOpen && (
+        <div className="steps">
+          <div className = "numbers"> 
+          {/* 2 - USE the state */}
+            <div className={step >= 1 ? 'active' : ''} onClick={() => setStep(step > 1  ? step - 1 : step)}>1</div>
+            <div className={step >= 2 ? 'active' : ''} onClick={() => setStep(2)}>2</div> 
+            <div className={step >= 3 ? 'active' : ''} onClick={() => setStep(step < 3  ? step + 1 : step)}>3</div>
+          </div>
+        <p className="message">
+          Step {step}: {messages[step -1]}
+          {/* {test.name} */}
+        </p>
+          <div className ="buttons">
+            <button 
+            style={buttonStyle} 
+            onClick={handlePrevious}>
+              Previous
+            </button>
+            <button 
+            style={buttonStyle} 
+            onClick={handleNext}>
+              Next
+            </button>
+          </div>
+        </div>)
+      }
     </div>
     </>
   );
 }
-
-export default App;
